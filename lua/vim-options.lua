16c5b6f8cec8a3b76b55f14e02a52056a7a9417e
NAI-JUI YEH <psxny2@exmail.nottingham.ac.uk>
2024-05-10 01:04:33 +0100

    init: finish init setup following tutorial tutorial: https://www.youtube.com/watch?v=4zyZ3sw_ulc

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.opt.number = true;
+vim.g.mapleader = " "
+vim.keymap.set('n', '<C-t>', ':tabs<CR>', {})
+vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
