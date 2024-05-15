a5fda06a27999e1295d985d365aa370f63300cf8
machichima <nary12321@gmail.com>
2024-05-15 23:35:52 +0100

    update toggleterm settings
    add split size for verital and horizontal
    add open_mapping

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,48 +1,63 @@
 -- referece for setup : https://medium.com/@shaikzahid0713/terminal-support-in-neovim-c616923e0431
 
 return {
-	"akinsho/toggleterm.nvim",
-	version = "*",
-	config = function()
-		require("toggleterm").setup({})
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
 
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
 
-		function _lazygit_toggle()
-			lazygit:toggle()
-		end
+      open_mapping = [[<c-\>]],
 
+    })
+
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    -- python
     local python = Terminal:new({ cmd = "python3", hidden = true })
 
-		function _PYTHON_TOGGLE()
-			python:toggle()
-		end
+    function _PYTHON_TOGGLE()
+      python:toggle()
+    end
 
-		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>tf",
-			"<cmd>ToggleTerm direction=float<cr><CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>tv",
-			"<cmd>ToggleTerm direction=vertical<cr><CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>th",
-			"<cmd>ToggleTerm direction=horizontal<cr><CR>",
-			{ noremap = true, silent = true }
-		)
-		vim.api.nvim_set_keymap(
-			"n",
-			"<leader>tp",
-			"<cmd>lua _PYTHON_TOGGLE()<cr>",
-			{ noremap = true, silent = true }
-		)
-	end,
+    local verSplit = Terminal:new({ direction = "vertical" })
+
+    function _VERTICAL_SPLIT()
+      verSplit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>tf",
+      "<cmd>ToggleTerm direction=float<CR>",
+      { noremap = true, silent = true }
+    )
+    -- vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>lua _VERTICAL_SPLIT()<cr>", { noremap = true, silent = true })
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>tv",
+      "<cmd>ToggleTerm direction=vertical<cr><CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>th",
+      "<cmd>ToggleTerm direction=horizontal<cr><CR>",
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap("n", "<leader>tp", "<cmd>lua _PYTHON_TOGGLE()<cr>", { noremap = true, silent = true })
+  end,
 }
