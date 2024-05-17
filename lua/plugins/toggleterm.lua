7e773f0eac6b60f7e69a98f102175f9672a5f66e
machichima <nary12321@gmail.com>
2024-05-17 23:16:03 +0100

    make lazygit be float term

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -19,7 +19,7 @@ return {
     })
 
     local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+    local lazygit = Terminal:new({ cmd = "lazygit", direction = "float", hidden = true })
 
     function _lazygit_toggle()
       lazygit:toggle()
