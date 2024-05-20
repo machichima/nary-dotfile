53474701a678ceffb894c591ff4b804fbba13bb9
machichima <nary12321@gmail.com>
2024-05-20 13:35:00 +0100

    add zz when cursor move
    keep the cursor in the middle of the screen when moving

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,3 +12,10 @@ vim.keymap.set('n', '<C-t>', ':tabs<CR>', {})
 vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
 vim.keymap.set('n', 'j', 'jzz')
 vim.keymap.set('n', 'k', 'kzz')
+
+vim.api.nvim_create_autocmd({"CursorMoved"}, {
+  callback = function ()
+    vim.api.nvim_feedkeys("zz", 'n', true)
+  end 
+
+})
