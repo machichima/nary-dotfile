5b6c426bf21589701631dcaafc05667bf724377a
machichima <nary12321@gmail.com>
2024-06-18 14:04:20 +0100

    feat: change line without breaking the word
    When sentence too long, the next "word" will go to next line
    originally the word will be seperate into two part

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.linebreak = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.hidden = true
