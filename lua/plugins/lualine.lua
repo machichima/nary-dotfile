25b7e2351e73c12c9c61fa9acc1371d7e12a75d2
machichima <nary12321@gmail.com>
2024-06-18 22:54:10 +0100

    feat[lualine]: change appearance of lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,12 +3,46 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "codedark"
-      }
+        theme = "codedark",
+        component_separators = "|",
+        section_separators = "",
+      },
+      -- sections = {
+      --   lualine_a = {
+      --     {
+      --       "buffers",
+      --       show_filename_only = true, -- Shows shortened relative path when set to false.
+      --       hide_filename_extension = false, -- Hide filename extension when set to true.
+      --       show_modified_status = true, -- Shows indicator when the buffer is modified.
 
+      --       mode = 0, -- 0: Shows buffer name
+      --       -- 1: Shows buffer index
+      --       -- 2: Shows buffer name + buffer index
+      --       -- 3: Shows buffer number
+      --       -- 4: Shows buffer name + buffer number
+
+      --       max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+      --       -- it can also be a function that returns
+      --       -- the value of `max_length` dynamically.
+      --       filetype_names = {
+      --         TelescopePrompt = "Telescope",
+      --         dashboard = "Dashboard",
+      --         packer = "Packer",
+      --         fzf = "FZF",
+      --         alpha = "Alpha",
+      --       }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
+
+      --       -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
+      --       use_mode_colors = false,
+
+      --       symbols = {
+      --         modified = " ●", -- Text to show when the buffer is modified
+      --         alternate_file = "#", -- Text to show to identify the alternate file
+      --         directory = "", -- Text to show when the buffer is a directory
+      --       },
+      --     },
+      --   },
+      -- },
     })
-
-  end
-
-
+  end,
 }
