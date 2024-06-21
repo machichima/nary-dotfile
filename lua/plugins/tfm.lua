928d80d4059fd10fb74c41dd6c4415f539535fae
machichima <nary12321@gmail.com>
2024-06-21 22:51:31 +0100

    feat[tfm]: replace nvim default file manager
    replace with ranger

--- lua/plugins/tfm.lua
+++ lua/plugins/tfm.lua
@@ -1,32 +1,43 @@
 return {
   "rolv-apneseth/tfm.nvim",
-  name = 'tfm',
+  name = "tfm",
+  lazy = false,
   opts = {
     file_manager = "ranger",
-    enable_cmds = true
+    enable_cmds = true,
+    replace_netrw = true,
   },
   keys = {
-        -- Make sure to change these keybindings to your preference,
-        -- and remove the ones you won't use
-        {
-            "<leader>e",
-            ":Tfm<CR>",
-            desc = "TFM",
-        },
-        {
-            "<leader>mh",
-            ":TfmSplit<CR>",
-            desc = "TFM - horizontal split",
-        },
-        {
-            "<leader>mv",
-            ":TfmVsplit<CR>",
-            desc = "TFM - vertical split",
-        },
-        {
-            "<leader>mt",
-            ":TfmTabedit<CR>",
-            desc = "TFM - new tab",
-        },
-    }
+    -- Make sure to change these keybindings to your preference,
+    -- and remove the ones you won't use
+    {
+      "<leader>e",
+      ":Tfm<CR>",
+      desc = "TFM",
+    },
+    {
+      "<leader>mh",
+      ":TfmSplit<CR>",
+      desc = "TFM - horizontal split",
+    },
+    {
+      "<leader>mv",
+      ":TfmVsplit<CR>",
+      desc = "TFM - vertical split",
+    },
+    {
+      "<leader>mt",
+      ":TfmTabedit<CR>",
+      desc = "TFM - new tab",
+    },
+  },
+
+  -- config = function()
+  --   vim.keymap.set('n', "<leader>e", function ()
+  --     local tfm = require("tfm")
+  --     local cwd = vim.fn.getcwd()
+  --     tfm.select_file_manager("ranger")
+  --     tfm.open(cwd .. "/", "tabedit")
+  --   end)
+  -- end
 }
