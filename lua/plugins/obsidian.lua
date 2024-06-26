227cc1e6486abf63d6c3ef1d0821b9084e4b1e8f
machichima <nary12321@gmail.com>
2024-06-26 12:16:54 +0100

    feat[obsidian]: disable link complete for markson
    use markson for link complete instead

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -22,7 +22,7 @@ local opts = {
   -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
   completion = {
     -- Set to false to disable completion.
-    nvim_cmp = true,
+    nvim_cmp = false,
     -- Trigger completion at 2 chars.
     min_chars = 2,
   },
