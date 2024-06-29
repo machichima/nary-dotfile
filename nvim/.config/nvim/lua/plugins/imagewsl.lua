a230b3f1caaeaf3c7830959f3290772ca7975d78
machichima <nary12321@gmail.com>
2024-06-29 16:03:31 +0100

    feat[all]: add all config into one dotfile

---
+++ nvim/.config/nvim/lua/plugins/imagewsl.lua
@@ -0,0 +1,15 @@
+return {
+  -- dir = "~/nvim_plugins/image-wezterm-tmux.nvim", -- Your path
+  -- name = "imagewsl",
+  -- config = function()
+  --   require("imagewsl").setup({
+  --     opts = {
+  --       open_type = "explorer",
+  --       -- option:
+  --       -- 1. "explorer": by Windows picture / tmux
+  --       -- 2. "tmux"
+  --       -- 3. "wsl": use `open` command
+  --     },
+  --   })
+  -- end,
+}
