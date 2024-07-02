72af8038534617cd2381c7eb60c9d29d26f17700
machichima <nary12321@gmail.com>
2024-07-02 21:16:56 +0100

    feat[nvim/molten]: auto detect python env

--- nvim/.config/nvim/lua/plugins/quarto.lua
+++ nvim/.config/nvim/lua/plugins/quarto.lua
@@ -92,6 +92,19 @@ return {
       },
       { "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "molten re-eval cell" },
     },
+    config = function()
+      require("molten").setup()
+      vim.keymap.set("n", "<leader>ip", function()
+        local venv = os.getenv("VIRTUAL_ENV")
+        if venv ~= nil then
+          -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+          venv = string.match(venv, "/.+/(.+)")
+          vim.cmd(("MoltenInit %s"):format(venv))
+        else
+          vim.cmd("MoltenInit python3")
+        end
+      end, { desc = "Initialize Molten for python3", silent = true })
+    end,
   },
 
   { -- Autoformat
