c35a22934a926633b433801fdfa6d62253a3e19d
machichima <nary12321@gmail.com>
2024-07-17 21:23:09 +0100

    fea[nvim/copilot]: change key for accept

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -2,5 +2,10 @@ return {
   "github/copilot.vim",
   config = function()
     -- require("copilot").setup()
+    vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
   end,
 }
