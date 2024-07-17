5b423dd9b1edf43ca617fdd4f8f578eccdfe97b0
machichima <nary12321@gmail.com>
2024-07-17 21:29:21 +0100

    feat[nvim/python]: config molten for ipynb(undone)

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -17,57 +17,46 @@ return {
       { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
     },
   },
+
   {
     "benlubas/molten-nvim",
-    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    enable = true,
     build = ":UpdateRemotePlugins",
     init = function()
-      -- these are examples, not defaults. Please see the readme
-      vim.g.molten_image_provider = "wezterm"
+      vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
-
-      -- I find auto open annoying, keep in mind setting this option will require setting
-      -- a keybind for `:noautocmd MoltenEnterOutput` to open the output again
-      -- vim.g.molten_auto_open_output = false
-
-      -- optional, I like wrapping. works for virt text and the output window
-      vim.g.molten_wrap_output = true
-
-      -- Output as virtual text. Allows outputs to always be shown, works with images, but can
-      -- be buggy with longer images
-      vim.g.molten_virt_text_output = true
-
-      -- this will make it so the output shows up below the \`\`\` cell delimiter
-      vim.g.molten_virt_lines_off_by_1 = true
+      vim.g.molten_auto_open_output = false
     end,
-
-    config = function()
-      -- require("molten-nvim").setup({})
-
-      vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
-      vim.keymap.set(
-        "n",
+    keys = {
+      { "<leader>mi", ":MoltenInit<cr>", desc = "[m]olten [i]nit" },
+      {
+        "<leader>mv",
+        ":<C-u>MoltenEvaluateVisual<cr>",
+        mode = "v",
+        desc = "molten eval visual",
+      },
+      {
         "<leader>me",
-        ":MoltenEvaluateOperator<CR>",
-        { silent = true, desc = "run operator selection" }
-      )
-      vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>", { silent = true, desc = "evaluate line" })
-      vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>", { silent = true, desc = "re-evaluate cell" })
-      vim.keymap.set(
-        "v",
-        "<leader>r",
-        ":<C-u>MoltenEvaluateVisual<CR>gv",
-        { silent = true, desc = "evaluate visual selection" }
-      )
+        ":MoltenEvaluateOperator<cr>",
+        mode = "n",
+        desc = "molten eval operator",
+      },
+      { "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "molten re-eval cell" },
+    },
+    config = function()
+      require("molten").setup()
+      vim.keymap.set("n", "<leader>ip", function()
+        local venv = os.getenv("VIRTUAL_ENV")
+        if venv ~= nil then
+          -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+          venv = string.match(venv, "/.+/(.+)")
+          vim.cmd(("MoltenInit %s"):format(venv))
+        else
+          vim.cmd("MoltenInit python3")
+        end
+      end, { desc = "Initialize Molten for python3", silent = true })
 
-      vim.keymap.set("n", "<leader>rd", ":MoltenDelete<CR>", { silent = true, desc = "molten delete cell" })
-      vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>", { silent = true, desc = "hide output" })
-      vim.keymap.set(
-        "n",
-        "<leader>os",
-        ":noautocmd MoltenEnterOutput<CR>",
-        { silent = true, desc = "show/enter output" }
-      )
+      vim.g.python3_host_prog = vim.fn.expand("/home/nary/.cache/pypoetry/virtualenvs/nvim-3B-PV_i1-py3.12/bin/python")
     end,
   },
   {
