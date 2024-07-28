f6c29e09d19fc4c6968788a3e35ce871c568090f
machichima <nary12321@gmail.com>
2024-07-28 21:29:33 +0100

    feat[nvim/debug]: add keys for debugging mode

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -25,7 +25,26 @@ return {
       dapui.close()
     end
 
-		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+
+    vim.keymap.set('n', "<F5>", dap.continue, {})
+    vim.keymap.set('n', "<F6>", dap.terminate, {})
+    vim.keymap.set('n', "<F10>", dap.step_over, {})
+    vim.keymap.set('n', "<F11>", dap.step_into, {})
+    vim.keymap.set('n', "<F12>", dap.step_out, {})
+    vim.keymap.set('n', "<Leader>dr", dap.repl.open, {})
+    vim.keymap.set('n', "<Leader>dl", dap.run_last, {})
+    vim.keymap.set({'n', 'v'}, "<Leader>dh",require('dap.ui.widgets').hover, {})
+    vim.keymap.set({'n', 'v'}, "<Leader>dp",require('dap.ui.widgets').preview, {})
+
+    vim.keymap.set('n', "<Leader>df", function()
+      local widgets = require('dap.ui.widgets')
+      widgets.centered_float(widgets.frames)
+    end, {})
+    vim.keymap.set('n', "<Leader>ds", function()
+      local widgets = require("dap.ui.widgets")
+      widgets.centered_float(widgets.scopes)
+    end, {})
   end,
 }
