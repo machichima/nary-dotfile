1cbbe049045a7443ce46458645083fcae9b2dc56
machichima <nary12321@gmail.com>
2024-08-21 19:39:32 +0100

    feat[nvim/quarto]: key for format table

--- nvim/.config/nvim/after/ftplugin/quarto/vim-options.lua
+++ nvim/.config/nvim/after/ftplugin/quarto/vim-options.lua
@@ -1,2 +1,6 @@
 vim.opt.textwidth=90
 vim.cmd("set colorcolumn=+1")
+
+-- formatting table
+-- ref: https://heitorpb.github.io/bla/format-tables-in-vim/
+vim.keymap.set("v", "<leader>tt", "! tr -s ' ' | column -t -s '|' -o '|'<CR>", { noremap = true, silent = true, desc = "format markdown table"})
