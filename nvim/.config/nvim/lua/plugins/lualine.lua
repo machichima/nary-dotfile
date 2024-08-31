198444a293298dd12cd091dbbc3f2c78d3a695e0
machichima <nary12321@gmail.com>
2024-08-31 11:47:26 +0100

    feat[nvim/lualine]: add macro&wordcount info

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
-return {
+packages_table = {
     "nvim-lualine/lualine.nvim",
     config = function()
-        local prose = require 'nvim-prose'
+        local prose = require("nvim-prose")
         require("lualine").setup({
             options = {
                 theme = "codedark",
@@ -9,48 +9,58 @@ return {
                 section_separators = "",
             },
             sections = {
+                lualine_b = {
+                    "branch",
+                    "diff",
+                    "diagnostics",
+                    {
+                        "macro-recording",
+                        fmt = show_macro_recording,
+                    },
+                },
                 lualine_x = {
-                    { prose.word_count,   cond = prose.is_available },
+                    { prose.word_count, cond = prose.is_available },
                     -- { prose.reading_time, cond = prose.is_available },
-                    'encoding', 'fileformat', 'filetype'
+                    "encoding",
+                    "fileformat",
+                    "filetype",
                 },
             },
-            -- sections = {
-            --   lualine_a = {
-            --     {
-            --       "buffers",
-            --       show_filename_only = true, -- Shows shortened relative path when set to false.
-            --       hide_filename_extension = false, -- Hide filename extension when set to true.
-            --       show_modified_status = true, -- Shows indicator when the buffer is modified.
-
-            --       mode = 0, -- 0: Shows buffer name
-            --       -- 1: Shows buffer index
-            --       -- 2: Shows buffer name + buffer index
-            --       -- 3: Shows buffer number
-            --       -- 4: Shows buffer name + buffer number
-
-            --       max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-            --       -- it can also be a function that returns
-            --       -- the value of `max_length` dynamically.
-            --       filetype_names = {
-            --         TelescopePrompt = "Telescope",
-            --         dashboard = "Dashboard",
-            --         packer = "Packer",
-            --         fzf = "FZF",
-            --         alpha = "Alpha",
-            --       }, -- Shows specific buffer name for that filetype ( { `filetype` = `buffer_name`, ... } )
-
-            --       -- Automatically updates active buffer color to match color of other components (will be overidden if buffers_color is set)
-            --       use_mode_colors = false,
-
-            --       symbols = {
-            --         modified = " ●", -- Text to show when the buffer is modified
-            --         alternate_file = "#", -- Text to show to identify the alternate file
-            --         directory = "", -- Text to show when the buffer is a directory
-            --       },
-            --     },
-            --   },
-            -- },
         })
     end,
 }
+
+function show_macro_recording()
+    local recording_register = vim.fn.reg_recording()
+    if recording_register == "" then
+        return ""
+    else
+        return "Recording @" .. recording_register
+    end
+end
+
+vim.api.nvim_create_autocmd("RecordingEnter", {
+    callback = function()
+        require("lualine").refresh({
+            place = { "statusline" },
+        })
+    end,
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+    callback = function()
+        local uv = vim.uv
+        local timer = uv.new_timer()
+        timer:start(
+            50,
+            0,
+            vim.schedule_wrap(function()
+                require("lualine").refresh({
+                    place = { "statusline" },
+                })
+            end)
+        )
+    end,
+})
+
+return packages_table
