5d339d66a95ad30d6f50b5b499df573d640fdb9f
machichima <nary12321@gmail.com>
2024-10-16 10:44:35 +0800

    feat[nvim]: remove auto session

--- nvim/.config/nvim/lua/plugins/autosession.lua
+++ nvim/.config/nvim/lua/plugins/autosession.lua
@@ -1,14 +1,14 @@
 return {
-	"rmagatti/auto-session",
-	config = function()
-		require("auto-session").setup({
-			log_level = "error",
-      auto_save_enabled = true,
-			cwd_change_handling = {
-				post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-					require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-				end,
-			},
-		})
-	end,
+	-- "rmagatti/auto-session",
+	-- config = function()
+	-- 	require("auto-session").setup({
+	-- 		log_level = "error",
+	--      auto_save_enabled = true,
+	-- 		cwd_change_handling = {
+	-- 			post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+	-- 				require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+	-- 			end,
+	-- 		},
+	-- 	})
+	-- end,
 }
