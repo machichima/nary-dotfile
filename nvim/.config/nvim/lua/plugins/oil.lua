a48a238915073d305d38a1638268db171ecbfc07
machichima <nary12321@gmail.com>
2024-10-16 10:45:41 +0800

    feat[nvim/oil]: show hidden folder

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,6 +1,14 @@
 return {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function ()
+        require("oil").setup({
+            view_options = {
+                show_hidden = true,
+            }
+        })
+    end
 }
