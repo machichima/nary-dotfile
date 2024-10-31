a5162c2b90ed446c28a7c6a6d0c2f39fc3cac179
machichima <nary12321@gmail.com>
2024-10-31 11:13:45 +0800

    feat[nvim/oil]: default hide hidden files

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
     config = function ()
         require("oil").setup({
             view_options = {
-                show_hidden = true,
+                show_hidden = false,
             }
         })
     end
