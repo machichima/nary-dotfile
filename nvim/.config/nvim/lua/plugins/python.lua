131bcd10c6fa451cc904374ad313097331cc1aef
machichima <nary12321@gmail.com>
2024-11-02 14:05:34 +0800

    feat[nvim]: update venv-selector+find poetry env

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,60 +1,31 @@
 -- This is for some more setup packages for Python
 
 return {
-  {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
-    config = function()
-      require("venv-selector").setup({
-        anaconda_envs_path = "~/miniconda3/envs",
-        dap_enabled = true,
-      })
-    end,
-    keys = {
-      -- Keymap to open VenvSelector to pick a venv.
-      { "<leader>vs", "<cmd>VenvSelect<cr>" },
-      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-    },
-  },
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+		branch = "regexp",
+		config = function()
+			require("venv-selector").setup({
+				anaconda_envs_path = "~/miniconda3/envs",
+				dap_enabled = true,
+				settings = {
+					search = {
+						poetry = {
+							command = "fd -p -g '**/bin/python' ~/workData/pypoetry/virtualenvs/",
+						},
+					},
+				},
+			})
+		end,
 
-  {
-    "benlubas/molten-nvim",
-    enable = true,
-    build = ":UpdateRemotePlugins",
-    init = function()
-      vim.g.molten_image_provider = "image.nvim"
-      vim.g.molten_output_win_max_height = 20
-      vim.g.molten_auto_open_output = false
-    end,
-    keys = {
-      { "<leader>mi", ":MoltenInit<cr>", desc = "[m]olten [i]nit" },
-      {
-        "<leader>mv",
-        ":<C-u>MoltenEvaluateVisual<cr>",
-        mode = "v",
-        desc = "molten eval visual",
-      },
-      {
-        "<leader>me",
-        ":MoltenEvaluateOperator<cr>",
-        mode = "n",
-        desc = "molten eval operator",
-      },
-      { "<leader>mr", ":MoltenReevaluateCell<cr>", desc = "molten re-eval cell" },
-    },
-    config = function()
-      require("molten").setup()
-      vim.keymap.set("n", "<leader>ip", function()
-        local venv = os.getenv("VIRTUAL_ENV")
-        if venv ~= nil then
-          -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
-          venv = string.match(venv, "/.+/(.+)")
-          vim.cmd(("MoltenInit %s"):format(venv))
-        else
-          vim.cmd("MoltenInit python3")
-        end
-      end, { desc = "Initialize Molten for python3", silent = true })
+		keys = {
+			-- Keymap to open VenvSelector to pick a venv.
+			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+			{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+		},
+	},
 
 	-- {
 	-- 	"benlubas/molten-nvim",
