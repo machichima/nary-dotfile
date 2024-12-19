60dc4f68e8d1dfa3256ca49d8af81d8fd4af7dab
machichima <nary12321@gmail.com>
2024-12-19 23:47:52 +0800

    nvim: update telescope+debugger
    telescope treesitter viewer
    
    debugger breakpoint style + debugpy debug source code

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -12,6 +12,14 @@ return {
 			local widgets = require("dap.ui.widgets")
 
 			require("dap-python").setup()
+			table.insert(require("dap").configurations.python, {
+				justMyCode = false, -- <--- insert here
+				type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+				request = "launch",
+				name = "debug source code",
+				program = "${file}",
+			})
+
 			dapui.setup()
 
 			dap.listeners.before.attach.dapui_config = function()
@@ -30,11 +38,11 @@ return {
 			vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
 			-- vim.keymap.set("n", "<Leader>dc", dap.continue, {})
 
-			vim.keymap.set("n", "<F5>", dap.continue, { desc = "dap start debugging or continue"})
-			vim.keymap.set("n", "<F6>", dap.terminate, { desc = "dap terminate"})
-			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "dap step over"})
-			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "dap step into"})
-			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "dap step out"})
+			vim.keymap.set("n", "<F5>", dap.continue, { desc = "dap start debugging or continue" })
+			vim.keymap.set("n", "<F6>", dap.terminate, { desc = "dap terminate" })
+			vim.keymap.set("n", "<F10>", dap.step_over, { desc = "dap step over" })
+			vim.keymap.set("n", "<F11>", dap.step_into, { desc = "dap step into" })
+			vim.keymap.set("n", "<F12>", dap.step_out, { desc = "dap step out" })
 
 			-- vim.keymap.set("n", "<Leader>dr", dap.repl.open, {})
 			-- vim.keymap.set("n", "<Leader>ds", function ()
@@ -229,25 +237,29 @@ return {
 			vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
 			vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
 
+			vim.api.nvim_set_hl(0, "DapBreakpointLine", { ctermbg = 0, bg = "#31353f" })
+			vim.api.nvim_set_hl(0, "DapLogPointLine", { ctermbg = 0, bg = "#31353f" })
+			vim.api.nvim_set_hl(0, "DapStoppedLine", { ctermbg = 0, bg = "#31353f" })
+
 			vim.fn.sign_define(
 				"DapBreakpoint",
-				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpointLine", numhl = "DapBreakpoint" }
 			)
 			vim.fn.sign_define(
 				"DapBreakpointCondition",
-				{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+				{ text = "ﳁ", texthl = "DapBreakpoint", linehl = "DapBreakpointLine", numhl = "DapBreakpoint" }
 			)
 			vim.fn.sign_define(
 				"DapBreakpointRejected",
-				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpointLine", numhl = "DapBreakpoint" }
 			)
 			vim.fn.sign_define(
 				"DapLogPoint",
-				{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+				{ text = "", texthl = "DapLogPoint", linehl = "DapLogPointLine", numhl = "DapLogPoint" }
 			)
 			vim.fn.sign_define(
 				"DapStopped",
-				{ text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+				{ text = "", texthl = "DapStopped", linehl = "DapStoppedLine", numhl = "DapStopped" }
 			)
 		end,
 	},
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,91 +1,102 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.6",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require("telescope.builtin")
-            local action = require("telescope.actions")
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			local action = require("telescope.actions")
 
-            -- builtin
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files in current workspace" })
-            vim.keymap.set("n", "<leader>fc", function()
-                builtin.find_files({
-                    cwd = vim.fn.expand("%:p:h"),
-                    hidden = true,
-                })
-            end, { desc = "Telescope: Find files in current directory" })
+			-- builtin
+			vim.keymap.set(
+				"n",
+				"<leader>ff",
+				builtin.find_files,
+				{ desc = "Telescope: Find files in current workspace" }
+			)
+			vim.keymap.set("n", "<leader>fc", function()
+				builtin.find_files({
+					cwd = vim.fn.expand("%:p:h"),
+					hidden = true,
+				})
+			end, { desc = "Telescope: Find files in current directory" })
 
-            vim.keymap.set("n", "<leader>gw", builtin.live_grep, { desc = "Telescope: Live Grep in current workspace" })
-            vim.keymap.set("n", "<C-f>", function()
-                builtin.live_grep({
-                    search_dirs = { vim.fn.expand("%:p") },
-                    hidden = true,
-                })
-            end, { desc = "Telescope: Live Grep in current file" })
+			vim.keymap.set("n", "<leader>gw", builtin.live_grep, { desc = "Telescope: Live Grep in current workspace" })
+			vim.keymap.set("n", "<C-f>", function()
+				builtin.live_grep({
+					search_dirs = { vim.fn.expand("%:p") },
+					hidden = true,
+				})
+			end, { desc = "Telescope: Live Grep in current file" })
 
-            vim.keymap.set("n", "<leader>gc", function()
-                builtin.live_grep({
-                    cwd = vim.fn.expand("%:p:h"),
-                    hidden = true,
-                })
-            end, { desc = "Telescope: Live Grep in current directory" })
+			vim.keymap.set("n", "<leader>gc", function()
+				builtin.live_grep({
+					cwd = vim.fn.expand("%:p:h"),
+					hidden = true,
+				})
+			end, { desc = "Telescope: Live Grep in current directory" })
 
-            vim.keymap.set("n", "<C-h>", builtin.help_tags, {})
-            vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+			vim.keymap.set(
+				"n",
+				"<leader>gn",
+				builtin.treesitter,
+				{ desc = "Telescope: Lists Function names, variables, from Treesitter" }
+			)
 
-            -- action
-            require("telescope").setup({
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<c-d>"] = action.delete_buffer,
-                        },
-                        n = {
-                            ["<c-d>"] = action.delete_buffer,
-                        },
-                    },
-                },
-            })
-        end,
-    },
+			vim.keymap.set("n", "<C-h>", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            -- This is your opts table
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({
-                            -- even more opts
-                        }),
-                    },
-                },
-            })
+			-- action
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<c-d>"] = action.delete_buffer,
+						},
+						n = {
+							["<c-d>"] = action.delete_buffer,
+						},
+					},
+				},
+			})
+		end,
+	},
 
-            require("telescope").load_extension("ui-select")
-        end,
-    },
-    {
-        "nvim-telescope/telescope-project.nvim",
-        config = function()
-            require("telescope").load_extension("project")
-        end,
-    },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").load_extension("file_browser")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			-- This is your opts table
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+					},
+				},
+			})
 
-            vim.keymap.set("n", "<leader>fb", function()
-                require("telescope").extensions.file_browser.file_browser({
-                    cwd = vim.fn.expand("%:p:h"),
-                    hidden = true,
-                })
-            end, { desc = "Telescope file browser: current directory" })
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-project.nvim",
+		config = function()
+			require("telescope").load_extension("project")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").load_extension("file_browser")
 
-        end,
-    }
+			vim.keymap.set("n", "<leader>fb", function()
+				require("telescope").extensions.file_browser.file_browser({
+					cwd = vim.fn.expand("%:p:h"),
+					hidden = true,
+				})
+			end, { desc = "Telescope file browser: current directory" })
+		end,
+	},
 }
