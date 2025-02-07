64852c12da30b2eb0c8390feb5a64f9f26d4f940
machichima <nary12321@gmail.com>
2025-02-07 22:04:31 +0800

    nvim: catppuccin line num color

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,11 +1,18 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-    })
-    vim.cmd.colorscheme("catppuccin")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "mocha",
+		})
+		vim.cmd.colorscheme("catppuccin")
+		-- Sets colors to line numbers Above, Current and Below  in this order
+		function LineNumberColors()
+			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#778E98", bold = false })
+			vim.api.nvim_set_hl(0, "LineNr", { fg = "#778E98", bold = false })
+			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#778E98", bold = false })
+		end
+        LineNumberColors()
+	end,
 }
