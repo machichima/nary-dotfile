1de65aea3555342d97f6bea423a95f8ded067917
machichima <nary12321@gmail.com>
2025-03-28 22:10:49 +0800

    bash: mound datalake/datalake2

--- mount_ahead_datalake.sh
+++ mount_ahead_datalake.sh
@@ -1,3 +1,1 @@
-# Nottingham gpu-cluster
-# sh -c "rclone --vfs-cache-mode writes mount \"ahead_ntu130\": ~/ahead-office"
 sh -c "sshfs naryyeh@office:/mnt/nas/data_lake ~/ahead-office/data_lake"
---
+++ mount_ahead_datalake2.sh
@@ -0,0 +1,1 @@
+sh -c "sshfs naryyeh@office:/mnt/nas/data_lake2 ~/ahead-office/data_lake2"
