6fced53e71243ea0a3d8c7fa01413387f281e9c5
machichima <nary12321@gmail.com>
2025-03-29 19:48:24 +0800

    nvim: udpate vim options for qf

--- nvim/.config/nvim/after/ftplugin/qf/vim-options.lua
+++ nvim/.config/nvim/after/ftplugin/qf/vim-options.lua
@@ -1,2 +1,32 @@
 vim.keymap.set("n", "<C-n>", ":cnext<CR>zz", { noremap = true, silent = true})
 vim.keymap.set("n", "<C-p>", ":cprev<CR>zz", { noremap = true, silent = true})
+
+vim.opt_local.textwidth = 90
+vim.cmd("setlocal colorcolumn=+1")
+
+-- formatting table
+-- ref: https://heitorpb.github.io/bla/format-tables-in-vim/
+vim.keymap.set(
+	"v",
+	"<leader>tt",
+	"! tr -s ' ' | column -t -s '|' -o '|'<CR>",
+	{ noremap = true, silent = true, buffer = true, desc = "format markdown table" }
+)
+
+
+vim.cmd([[
+  syntax region HighlightEquals start=/==/ end=/==/
+  highlight HighlightEquals guifg=yellow ctermfg=yellow
+
+  syntax region HighlightBold start=/\*\*/ end=/\*\*/
+  highlight HighlightBold guifg=blue ctermfg=blue
+]])
+
+
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = false,
+    update_in_insert = false,
+    severity_sort = true,
+})
