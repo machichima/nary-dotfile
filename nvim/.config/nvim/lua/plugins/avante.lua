d83386e8db35de813a03778b529767a7032c7b8d
machichima <nary12321@gmail.com>
2025-08-26 10:00:56 +0800

    nvim: comment out avante

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,67 +1,67 @@
 return {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-        provider = "copilot",
-        vendors = {
-            deepseek = {
-                __inherited_from = "openai",
-                api_key_name = "DEEPSEEK_API_KEY",
-                endpoint = "https://api.deepseek.com",
-                model = "deepseek-coder",
-                max_tokens = 8192,
-            },
-        },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        {
-            -- support for image pasting
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                -- recommended settings
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    -- required for Windows users
-                    use_absolute_path = true,
-                },
-            },
-        },
-        -- {
-        --     -- Make sure to set this up properly if you have lazy=true
-        --     'MeanderingProgrammer/render-markdown.nvim',
-        --     opts = {
-        --         file_types = { "markdown", "Avante" },
-        --         heading = {
-        --
-        --             -- Turn on / off heading icon & background rendering.
-        --             enabled = false,
-        --             -- Additional modes to render headings.
-        --             render_modes = false,
-        --             -- Turn on / off atx heading rendering.
-        --             atx = false,
-        --             -- Turn on / off setext heading rendering.
-        --             setext = false,
-        --             -- Turn on / off any sign column related rendering.
-        --             sign = false,
-        --         },
-        --
-        --     },
-        --     ft = { "markdown", "Avante" },
-        -- },
-    },
+    -- "yetone/avante.nvim",
+    -- event = "VeryLazy",
+    -- version = false, -- Never set this value to "*"! Never!
+    -- opts = {
+    --     provider = "copilot",
+    --     providers = {
+    --         deepseek = {
+    --             __inherited_from = "openai",
+    --             api_key_name = "DEEPSEEK_API_KEY",
+    --             endpoint = "https://api.deepseek.com",
+    --             model = "deepseek-coder",
+    --             max_tokens = 8192,
+    --         },
+    --     },
+    -- },
+    -- -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- build = "make",
+    -- -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    -- dependencies = {
+    --     "nvim-treesitter/nvim-treesitter",
+    --     "stevearc/dressing.nvim",
+    --     "nvim-lua/plenary.nvim",
+    --     "MunifTanjim/nui.nvim",
+    --     --- The below dependencies are optional,
+    --     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    --     {
+    --         -- support for image pasting
+    --         "HakonHarnes/img-clip.nvim",
+    --         event = "VeryLazy",
+    --         opts = {
+    --             -- recommended settings
+    --             default = {
+    --                 embed_image_as_base64 = false,
+    --                 prompt_for_file_name = false,
+    --                 drag_and_drop = {
+    --                     insert_mode = true,
+    --                 },
+    --                 -- required for Windows users
+    --                 use_absolute_path = true,
+    --             },
+    --         },
+    --     },
+    --     -- {
+    --     --     -- Make sure to set this up properly if you have lazy=true
+    --     --     'MeanderingProgrammer/render-markdown.nvim',
+    --     --     opts = {
+    --     --         file_types = { "markdown", "Avante" },
+    --     --         heading = {
+    --     --
+    --     --             -- Turn on / off heading icon & background rendering.
+    --     --             enabled = false,
+    --     --             -- Additional modes to render headings.
+    --     --             render_modes = false,
+    --     --             -- Turn on / off atx heading rendering.
+    --     --             atx = false,
+    --     --             -- Turn on / off setext heading rendering.
+    --     --             setext = false,
+    --     --             -- Turn on / off any sign column related rendering.
+    --     --             sign = false,
+    --     --         },
+    --     --
+    --     --     },
+    --     --     ft = { "markdown", "Avante" },
+    --     -- },
+    -- },
 }
